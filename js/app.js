337bb0f02d9c2be1d18f8f7e842c25bbfb0a1162
Adam Richardson <ajrich01@gmail.com>
2017-05-13 14:18:44 -0400

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function loadUser() {
+  var template = $('#template').html();
+  Mustache.parse(template);   // optional, speeds up future uses
+  var rendered = Mustache.render(template, {name: "Luke"});
+  $('#target').html(rendered);
+}
