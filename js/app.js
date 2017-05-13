321159d54fbbd1cef88ce1411ec6e0c3975409b6
Adam Richardson <ajrich01@gmail.com>
2017-05-13 18:54:47 -0400

    Removed jquery and made basic form for setting puzzle size

--- js/app.js
+++ js/app.js
@@ -1,6 +1,41 @@
 function loadUser() {
-  var template = $('#template').html();
+  var template = document.getElementById("template").innerHTML;
   Mustache.parse(template);   // optional, speeds up future uses
   var rendered = Mustache.render(template, {name: "Luke"});
-  $('#target').html(rendered);
+  document.getElementById("target").innerHTML = rendered
+}
+
+var min = 5;
+var max = 20;
+var width = min;
+var height = min;
+var puzzle = new Array(width*height);
+puzzle.fill(false);
+
+function setSize() {
+  width = document.getElementById("width").value;
+  height = document.getElementById("height").value;
+
+  puzzle = new Array(width * height);
+  puzzle.fill(false);
+  console.log("Width: ", width);
+  console.log("Height: ", height);
+  console.log("Puzzle", puzzle);
+}
+
+function minMaxCheck(object) {
+  if(object.value > max){
+    object.value = max;
+  }
+  if(object.value < min) {
+    object.value = min;
+  }
+}
+
+function selectItem(row, col) {
+
+}
+
+function renderPuzzle() {
+  console.log("Rendering puzzle");
 }
